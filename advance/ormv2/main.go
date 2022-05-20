3661e8d74811d96602494594a73ee2e9b958cb95
chieund <c_nguyen@lab.allabout.co.jp>
2022-05-20 13:45:56 +0700

    second orm

--- advance/ormv2/main.go
+++ advance/ormv2/main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"gorm-test/controllers"
+	"log"
+	"os"
 )
 
 func main() {
+	LOG_FILE := "log/log"
+
+	logWriter, err := os.OpenFile(LOG_FILE, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatalln("Unable to set logfile:", err.Error())
+	}
+
+	log.SetFlags(log.Lshortfile)
+
+	log.SetOutput(logWriter)
+	log.Println("This is a log form GoLang")
+
 	controllers.Init()
 }
